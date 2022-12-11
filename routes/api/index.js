a6a26e27a41cf9f3bfadd76bcafd44e8ec58bbad
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-12-10 19:05:56 -0500

    added user model

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+//APi Endpoints
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+//API Endpoints
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
